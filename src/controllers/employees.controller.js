51bc52d385b5bc5d89030c3c761015f973d718e2
CuauCabrera <cabrera.cuau@gmail.com>
2023-10-17 23:53:28 -0600

    Setup PATCH request

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -34,5 +34,14 @@ export const deleteEmployee = async(req, res) => {
 
 
 
-export const updateEmployee = (req, res) => res.send('Updating employee data');
+export const updateEmployee = async(req, res) => {
+    const {id} = req.params
+    const {name, salary} = req.body
+    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name,salary,id])
+    if (result.affectedRows === 0) return res.status(404).json({
+        message: 'Employee not found'
+    })
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+    res.json(rows[0])
+};
 
