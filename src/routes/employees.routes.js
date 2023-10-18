82fe01e8f15e89f1b6dc704fa97237e83d780910
CuauCabrera <cabrera.cuau@gmail.com>
2023-10-17 23:21:55 -0600

    Setup DELETE request

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -8,6 +8,6 @@ router.get('/employees/:id',getEmployeeId);
 router.get('/employees', getEmployee);
 router.post('/employees', createEmployee);
 router.put('/employees', updateEmployee);
-router.delete('/employees', deleteEmployee);
+router.delete('/employees/:id', deleteEmployee);
 
 export default router
